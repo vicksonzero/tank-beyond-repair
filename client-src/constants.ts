5aac31ca7d8e80dacb092119ec22471e67aa6e6f
Chui Hin Wah (Dickson) <vicksonzero@users.noreply.github.com>
2020-02-02 12:27:33 +0800

    Item destroy timer

--- client-src/constants.ts
+++ client-src/constants.ts
@@ -4,3 +4,5 @@ export const WORLD_WIDTH = 1366;
 export const WORLD_HEIGHT = 768
 export const BASE_LINE_WIDTH = 100;
 export const BULLET_SPEED = 2;
+export const ITEM_LIFESPAN = 10 * 1000;
+export const ITEM_LIFESPAN_WARNING = 7 * 1000;
